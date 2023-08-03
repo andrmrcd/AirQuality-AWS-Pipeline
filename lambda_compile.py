2e2fbc3b9a136368f35a387f1af1ef373169e8bd
dbop01 <76557984+andrmrcd@users.noreply.github.com>
2023-08-03 15:19:20 +0800

    Refactor Lambda function to handle header row

--- lambda_compile.py
+++ lambda_compile.py
@@ -16,15 +16,26 @@ def lambda_handler(event,context):
     new_data = response["Body"].read().decode("utf-8")
 
     # Read existing data from destination (if it exists)
+    header_row = False
     try:
         existing_data_response = s3.get_object(Bucket=outputbucket, Key=outputfile)
         existing_data = existing_data_response["Body"].read().decode("utf-8")
+        header_row = True
     except s3.exceptions.NoSuchKey:
         # If csv file doesn't exist, create empty string
         existing_data = ''
 
+    # If existing_data exists
+    if header_row == True:
+        # Remove header row from new_data
+        striped_data = new_data.strip().split('\n')
+        new_data_without_header = striped_data[1:]
+        upload_data = '\n'.join(new_data_without_header)
+    else:
+        upload_data = new_data
+
     # Combine existing data with newly uploaded data
-    combined_data = existing_data + '\n' + new_data
+    combined_data = existing_data + upload_data
 
     # Upload the combined data to the destination bucket
     s3.put_object(Bucket=outputbucket, Key=outputfile,Body=combined_data) 
